80462c2c079aeecb24adfe2844bbcdcc4a05ea22
calin2244 <bastureacalin@gmail.com>
2023-09-30 12:52:45 +0400

    Problem 338 LeetCode

--- test_p.cpp
+++ test_p.cpp
@@ -1,8 +1,34 @@
 #include <iostream>
+#include <vector>
+
+/*
+    *338. Counting Bits
+*/
+class Solution {
+public:
+    static std::vector<int> countBits(int n) {
+        std::vector<int> bits;
+        bits.reserve(n + 1);
+        for(int curr = 0; curr <= n; ++curr){
+            int sum = 0, copy = curr;
+            while(copy){
+                sum += (copy & 1 ? 1 : 0);
+                copy >>= 1;
+            }
+
+            bits.push_back(sum);
+        }
+
+        return bits;
+    }
+};
 
 int main(){
-    /*
-        *Nothing
-    */
+    std::vector<int> res = Solution::countBits(10);
+    for(const auto& el: res){
+        std::cout << el << ' ';
+    }
+
+    std::cout << '\n';
     return 0;
 }
